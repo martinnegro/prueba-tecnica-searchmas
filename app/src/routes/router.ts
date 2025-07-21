27576359d15c4a09f9ef6728c0a59bd4c8c54ee3
Martín Negro Ruffino <mnegroruffino@gmail.com>
2025-07-21 02:02:57 +0000

    feat: Add export csv funcionality

--- app/src/routes/router.ts
+++ app/src/routes/router.ts
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import externalDataController from "@/controllers/externalData.controller";
 import getStationsController from "@/controllers/getStations.controller";
+import exportCsvController from "@/controllers/exportCsv.controller";
 
 const router = Router();
 
 router.post('/external-data', externalDataController)
-
 router.get('/data', getStationsController)
-
-router.get('/export-csv',async (req, res) => {
-
-})
+router.get('/export-csv',exportCsvController);
 
 export default router;
