41ef97cead7c1b66b703de0f341230a4af0e0e25
Martín Negro Ruffino <mnegroruffino@gmail.com>
2025-07-22 00:35:18 -0300

    fix: Add response on successful external data update

--- app/src/controllers/externalData.controller.ts
+++ app/src/controllers/externalData.controller.ts
@@ -5,6 +5,7 @@ import updateStationsDataFromApiService from '@/services/updateStationsDataFromA
 export default async function externalDataController(_req: Request, res: Response) {
     try {
         await updateStationsDataFromApiService();
+        res.status(200).send('Datos de estaciones actualizados correctamente');
     } catch (error) {
         console.error('Error en externalDataController:', error);
         res.status(500).send('Error al actualizar los datos de las estaciones');
