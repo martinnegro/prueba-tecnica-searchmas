cd001ea2ee8de714cde6e782ed07d728abb7f37d
Martín Negro Ruffino <mnegroruffino@gmail.com>
2025-07-22 03:36:50 -0300

    fix: Fix import

--- app/src/controllers/getStations.controller.ts
+++ app/src/controllers/getStations.controller.ts
@@ -1,4 +1,4 @@
-import getStationsService from '@/services/getStationsFromDb.service';
+import { getStationsService } from '@/services/getStationsFromDb.service';
 import { Request, Response } from 'express';
 
 export async function getStationsController(_req: Request, res: Response) {
